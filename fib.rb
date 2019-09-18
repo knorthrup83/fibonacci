26d1f39ab149bf2832333401c09afe83a97c99df
knorthrup83@gmail.com <knorthrup83@gmail.com>
2019-09-18 18:15:49 +0000

    Initial commit

---
+++ fib.rb
@@ -0,0 +1,13 @@
+def recursive_fib
+	
+end
+
+def iterative_fib
+end
+
+# require 'benchmark'
+# num = 35
+# Benchmark.bm do |x|
+#   x.report("recursive_fib") { recursive_fib(num) }
+#   x.report("iterative_fib")  { iterative_fib(num)  }
+# end
